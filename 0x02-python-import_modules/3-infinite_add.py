fc2ea6bb5eb87f085bdfecc3920c7197e93a67fb
Mulih <almulih@yahoo.com>
2023-03-21 13:55:48 -0700

    tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv[1:]
+argv_count = len(argv)
+index = 1
+result = 0
+while index <= argv_count:
+    result += int(sys.argv[index])
+    index += 1
+print("{:d}".format(result))
