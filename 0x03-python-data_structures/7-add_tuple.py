7d1875f511894b8cfecc705ea3160c350ab588a1
Mulih <almulih@yahoo.com>
2023-11-28 05:25:38 -0800

    re-do task 3

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a=(), tuple_b()):
-    """Add two tuples."""
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
-        else:
-            tuple_b = tuple_b[0], 0
+def add_tuple(tuple_a=(), tuple_b=()):
+    tup_a = check_tuple(tuple_a)
+    tup_b = check_tuple(tuple_b)
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return((tup_a[0] + tup_b[0]), (tup_a[1] + tup_b[1]))
+
+
+def check_tuple(tuple=()):
+    length = len(tuple)
+    if length == 1:
+        tuple = (tuple[0], 0)
+    elif length >= 2:
+        tuple = (tuple[0], tuple[1])
+    elif length == 0:
+        tuple = (0, 0)
+
+    return tuple
