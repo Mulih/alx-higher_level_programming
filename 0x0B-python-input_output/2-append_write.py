f15c5abb77692f05f462472ab26ee49f01c9be0c
Mulih <almulih@yahoo.com>
2023-12-15 01:36:48 -0800

    upoload of task 0 - 5 & README"

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a file-appending fnction."""
+
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (Str): The string to append to the file.
+    Return:
+        The number of characters appended.
+    """
+    with open(filename="", encoding="UTF8") as f:
+        return f.write(text)
