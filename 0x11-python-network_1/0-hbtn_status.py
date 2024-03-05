5e6b58397a2dd6a1cc4b62621d6e40de3abfae3e
Mulih <almulih@yahoo.com>
2024-03-05 03:44:13 -0800

    upload of conditional loop instead of the function intially created

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-This script fetches the status from https://alx-intranet.hbtn.io/status
-"""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-def fetch_status():
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
 
 if __name__ == "__main__":
-    fetch_status()
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
