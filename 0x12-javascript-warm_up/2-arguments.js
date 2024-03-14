d26ccf9e614bc171d51b18263e5295b2922c8141
Mulih <almulih@yahoo.com>
2024-03-14 09:20:18 +0300

    change 2-arguments.js script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 const count = process.argv.length;
 
-if (process.argv.length === 2) {
+if (process.argv.length <= 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
